311c86fa50e655409dc3b603cd1a93f26430174b
polinabatanova <polab98@gmail.com>
2026-05-06 11:30:30 +0100

    Renamed middleware to proxy

--- proxy.js
+++ proxy.js
@@ -9,7 +9,7 @@ const ROLE_ROUTES = {
     '/events/create': ['organiser', 'admin'],
 };
 
-export function middleware(request) {
+export function proxy(request) {
     const { pathname } = request.nextUrl;
     const cookie = request.cookies.get(COOKIE_NAME);
 
